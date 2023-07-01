7c35b1264df1c8055b029a6853ad8a1b24e2c06a
yogeshrepo <118822006+yogeshrepo@users.noreply.github.com>
2023-07-01 17:04:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	let table={
+		"2": "abc",
+		"3": "def",
+		"4": "ghi",
+		"5": "jkl",
+		"6": "mno",
+		"7": "pqrs",
+		"8": "tuv",
+		"9": "wxyz"
+	}
+ 
+	let solution=[];
+ 
+	function solve(currentIndex, calculatedString){
+		if(input.length<= currentIndex){
+			solution.push(calculatedString);
+			return;
+		}
+ 
+		let currentChar = input[currentIndex];
+		for(let i=0;i< table[currentChar].length;i++){
+			solve(currentIndex +1, calculatedString + table[currentChar][i]);
+		}
+	}
+	solve(0, "");
+ 
+	return(solution);
 }
 
 module.exports = letterCombinations;
